8459bfa3a423199402ee8aa6e2446a5bcd376576
kusfaizlul <kuszocool@gmail.com>
2020-10-18 14:32:32 +0000

    First code Ask user to Input

---
+++ userinput.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+	int age = 0;
+	printf("Yow Whatdup !!");
+	printf("How old are you? >");
+	scanf("%d",&age);
+	printf("Your are %d years old",age);
+	return(0);
+	/*In this lab seasion we learn how to get input from user */
+}
